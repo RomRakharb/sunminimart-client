bd08a205bced56b468f5b7802e50ea7259ff96df
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-01-13 14:57:58 +0700

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,12 +5,39 @@ echo "---------------------"
 echo "starts install script"
 echo "---------------------"
 echo
-sudo apt update && sudo apt -y upgrade
-sudo apt-get install -y software-properties-common
-sudo apt update
-sudo add-apt-repository -y ppa:maveonair/helix-editor
-sudo apt update
-sudo apt install helix
+sudo apt-get update && apt-get install -y \   
+    pkg-config \    
+    libasound2-dev \
+    libudev-dev \   
+    mesa-utils \ 
+    vulkan-tools \
+    libwayland-dev \
+    libxkbcommon-dev \   
+    libvulkan1 \    
+    libvulkan-dev \ 
+    libegl1-mesa-dev \   
+    libgles2-mesa-dev \  
+    libx11-dev \    
+    libxcursor-dev \
+    libxrandr-dev \
+    libxi-dev \
+    libxrandr-dev \
+    libxcb1-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-randr0-dev \
+    libxcb-shape0-dev \
+    libxcb-xfixes0-dev \
+    libxcb-xkb-dev \
+    libegl1-mesa \
+    libgl1-mesa-glx \
+    libgl1-mesa-dri \
+    libglu1-mesa-dev \
+    libglu1-mesa \
+    libgles2-mesa \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 echo
 echo "---------------------"
 echo "install script ends"
